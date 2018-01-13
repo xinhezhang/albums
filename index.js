7902fa388890108dd5cb57ae4e50d0cac9ed8ca7
xinhezhang <xinhezhang@hotmail.com>
2018-01-13 03:45:26 -0800

    register App component using differnt syntax

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { Text, AppRegistry } from 'react-native';
 
 // create a component
-const App = () => {
-  return (
-    <Text>Hello World</Text>
-  );
-};
+const App = () => (
+  <Text>Hello World</Text>
+);
+// const App = () => {
+//   return (
+//     <Text>Hello World</Text>
+//   );
+// };
 
 // render to the device
 AppRegistry.registerComponent('albums', () => App);
