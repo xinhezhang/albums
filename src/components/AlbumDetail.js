890ef373dd1141fc8cab9228b0d1ef05cedcb568
xinhezhang <xinhezhang@hotmail.com>
2018-01-14 01:57:36 -0800

    apply styles for AlbumDetail, image not display

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
@@ -8,10 +8,23 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image source={{ uri: props.album.thumbnail_image }} />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>Title: {props.album.title}</Text>
+          <Text>Artist: {props.album.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
 };
 
 export default AlbumDetail;
