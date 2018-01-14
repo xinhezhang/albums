451358693b7f0e3fc3aef07872c3df9e03690f6b
xinhezhang <xinhezhang@hotmail.com>
2018-01-14 02:06:11 -0800

    apply styles for title and thumbnail container

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,20 +6,25 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   const { thumbnail_image, title, artist } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
-  
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle
+  } = styles;
+
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>Title: {title}</Text>
-          <Text>Artist: {artist}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -31,9 +36,18 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  headerTextStyle: {
+    fontSize: 20,
+  },
   thumbnailStyle: {     // apply for image (must have)
     height: 50,
     width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
   },
 };
 
