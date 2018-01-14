75920ac5b3a390aa1d3d787b92664688c7aa1424
xinhezhang <xinhezhang@hotmail.com>
2018-01-14 01:31:36 -0800

    apply styles to CardSection component

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { View } from 'react-native';
 
 const CardSection = () => {
+  const { containerStyle } = styles;
+
   return (
-    <View>
+    <View style={containerStyle}>
     </View>
   );
 }
+
+const styles = {
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative',
+  },
+};
 
 export default CardSection;
