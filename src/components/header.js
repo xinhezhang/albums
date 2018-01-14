6a2e05ef3984312c1660a7926cc2cb5d621ef6ed
xinhezhang <xinhezhang@hotmail.com>
2018-01-13 17:05:20 -0800

    add View for Header and set background color

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
+
   return (
-    <Text style={textStyle}>
-      Albums
-    </Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
   );
 };
 
 const styles = {
   textStyle: {
-    fontSize: 20
-  }
+    fontSize: 20,
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+  },
 };
 
 export default Header;
